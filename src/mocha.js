c670fc147f560d452df920cc9d8b76da6cacb7b6
Brent Anderson <me@brentjanderson.com>
2014-12-02 16:57:56 -0700

    Update to use mocha for running tests, much better output and error diagnosis.

---
+++ src/mocha.js
@@ -0,0 +1,260 @@
+var chai		= require('chai'),
+	expect		= chai.expect,
+	fs			= require('fs'),
+	docData		= fs.readFileSync('testcase.json', 'utf8'),
+	doc;
+
+function noDecimals(v) {
+	return v % 1 === 0.0;
+}
+
+function stringMatch(stringValues) {
+	return function(testValue) {
+		for (var i = stringValues.length - 1; i >= 0; i--) {
+			if (stringValues[i] === testValue) {
+				return true;
+			}
+		}
+		return false;
+	};
+}
+
+function domainExists(domains) {
+	// Verify the domain assigned exists
+	return function(domainId) {
+		for (var i = domains.length - 1; i >= 0; i--) {
+			if (domainId === domains[i].id) {
+				return true;
+			}
+		}
+		return false;
+	};
+}
+
+function stepExists(steps) {
+	return function(stepId) {
+		// Verify the step exists
+		for (var i = steps.length - 1; i >= 0; i--) {
+			if (stepId === steps[i].id) {
+				return true;
+			}
+		}
+		return false;
+	};
+}
+
+// Steps validation
+function predecessorTest(pred) {
+	return function() {
+		it('should have a property id that is a string and is a real step id', function() {
+			expect(pred).to.have.property('id')
+			.that.is.a('string')
+			.and.to.satisfy(stepExists(doc.steps));
+		});
+
+		it('should have a property type that is a string and is either normal_relationship or loose_temporal_relationship', function() {
+			expect(pred).to.have.property('type')
+			.that.is.a('string')
+			.and.to.satisfy(stringMatch(['normal_relationship', 'loose_temporal_relationship']));
+		});
+
+		it('can have an optional property title that is a string', function() {
+			if (pred.title) {
+				expect(pred).to.have.property('title')
+				.that.is.a('string');
+			}
+		});
+	};
+}
+
+function stepProblemTest(problem) {
+	return function() {
+		it('should be an object', function() {
+			expect(problem).to.be.an('object');
+		});
+
+		it('should have property type that is a string and is one of inconvenient, confusing, difficult, or likely_to_fail', function() {
+			expect(problem).to.have.property('type')
+			.that.is.a('string')
+			.and.to.satisfy(stringMatch(['inconvenient', 'confusing', 'difficult', 'likely_to_fail']));
+		});
+
+		it('should have property description that is a string', function() {
+			expect(problem).to.have.property('description')
+			.that.is.a('string');
+		});
+	};
+}
+
+function stepTest(step) {
+	return function() {
+		it('should be an object', function() {
+			expect(step).to.be.an('object');
+		});
+
+		it('should have property id that is a string', function() {
+			expect(step).to.have.property('id')
+			.that.is.a('string');
+		});
+
+		it('should have a property title that is a string', function() {
+			expect(step).to.have.property('title')
+			.that.is.a('string');
+		});
+
+		it('should have a property type that is a string and one of process, decision, divergent_process, or wait', function() {
+			expect(step).to.have.property('type')
+			.that.is.a('string')
+			.and.to.satisfy(stringMatch(['process', 'decision', 'divergent_process', 'wait']));
+		});
+
+		it('can have an optional property emphasized that is a boolean', function() {
+			if (step.emphasized) {
+				expect(step).to.have.property('emphasized')
+				.that.is.a('boolean');
+			}
+		});
+
+		it('can have an optional property value_specific that is a number and has no decimals', function() {
+			if (step.value_specific) {
+				expect(step).to.have.property('value_specific')
+				.that.is.a('Number')
+				.and.to.satisfy(noDecimals);
+			}
+		});
+
+		it('can have an optional property value_generic that is a number and has no decimals', function() {
+			if (step.value_generic) {
+				expect(step).to.have.property('value_generic')
+				.that.is.a('Number')
+				.and.to.satisfy(noDecimals);
+			}
+		});
+
+		if (step.predecessors) {
+			it('can have an optional property predecessors that is an array', function() {
+				expect(step).to.have.property('predecessors')
+				.that.is.an('array');
+			});
+
+			for (var p = step.predecessors.length - 1; p >= 0; p--) {
+				describe('Predecessor at index ' + p, predecessorTest(step.predecessors[p]));
+			}
+		}
+
+		it('should have property domain that is an object', function() {
+			expect(step).to.have.property('domain')
+			.that.is.an('object');
+		});
+
+		describe("Step id " + step.id + " domain", function() {
+			var domain = step.domain;
+
+			it('should have property id that is a string and is an existing domain', function() {
+				expect(domain).to.have.property('id')
+				.that.is.a('string')
+				.and.to.satisfy(domainExists(doc.domains));
+			});
+
+			it('should have property region that is an object', function() {
+				expect(domain).to.have.property('region')
+				.that.is.an('object');
+			});
+
+			describe("Region", function() {
+				it('should have property type that is a string and one of independent, surrogate, direct_leading, or direct_shared', function() {
+					expect(domain.region).to.have.property('type')
+					.that.is.a('string')
+					.and.to.satisfy(stringMatch(['independent', 'surrogate', 'direct_leading', 'direct_shared']));
+				});
+
+				if (domain.region.type !== 'independent') {
+					it('when non-independent should have property with_domain that is a string that is a valid domain', function() {
+						expect(domain.region).to.have.property('with_domain')
+						.that.is.a('string')
+						.and.to.satisfy(domainExists);
+					});
+				}
+			});
+		});
+
+		if (step.problems) {
+			it('can have optional property problems that is an array', function() {
+				expect(step).to.have.property('problems')
+				.that.is.an('array');
+			});
+
+			for (var pr = 0; pr < step.problems.length; pr++) {
+				describe('Step problem at index ' + pr, stepProblemTest(step.problems[pr]));
+			}
+		}
+	};
+}
+
+// Document must be valid JSON
+describe('PCN Document', function() {
+	it('must be valid JSON', function() {
+		expect(function() { JSON.parse(docData); }).to.not.throw(Error);
+	});
+
+	doc = JSON.parse(docData);
+
+	// Metadata must be object
+	it('must have valid metadata', function() {
+		expect(doc).to.have.property('metadata')
+		.that.is.an('object', 'The metadata property must be an object');
+	});
+
+	it('should have domains property that is an array', function() {
+		// Domains is array
+		expect(doc).to.have.property('domains')
+		.that.is.an('array', 'The domains property must be an array');
+	});
+
+	
+	it('should have steps property that is an array', function() {
+		// Steps is array of at least 2 objects
+		expect(doc).to.have.property('steps')
+		.that.is.an('array', 'The steps property must be an array');
+	});
+
+	describe('Domains', function() {
+		// Domain Validation
+		function domainTest() {
+			var domain = doc.domains[d];
+			/**
+			1. id is string
+			2. id is unique
+			3. title is string
+			4. subtitle is string
+			**/
+			it('should be an object', function() {
+				expect(domain).to.be.an('object');
+			});
+
+			it('should have property id that is a string', function() {
+				expect(domain).to.have.property('id')
+				.that.is.a('string');
+			});
+
+			it('should have property title that is a string', function() {
+				expect(domain).to.have.property('title')
+				.that.is.a('string');
+			});
+
+			it('should have property subtitle that is a string', function() {
+				expect(domain).to.have.property('subtitle')
+				.that.is.a('string', 'Domain ' + d + ' title must be a string');
+			});
+		}
+		for (var d = 0; d < doc.domains.length; d++) {
+			describe('Domain at index ' + d, domainTest);
+		}
+	});
+
+	describe('Steps', function() {
+		for (var s = 0; s < doc.steps.length; s++) {
+			describe('Step at index ' + s, stepTest(doc.steps[s]));
+		}
+	});
+});
--- src/pcnlint.js
+++ src/pcnlint.js
@@ -2,180 +2,29 @@
 	"use strict";
 
 	// Load PCN document from parameter
-	var fs		= require('fs'),
-		stdin 	= require('get-stdin'),
-		chai	= require('chai'),
-		expect	= chai.expect;
-
-	function noDecimals(v) {
-		return v % 1 === 0.0;
-	}
-
-	function stringMatch(stringValues) {
-		return function(testValue) {
-			for (var i = stringValues.length - 1; i >= 0; i--) {
-				if (stringValues[i] === testValue) {
-					return true;
-				}
-			}
-			return false;
-		};
-	}
-
-	function domainExists(domains) {
-		// Verify the domain assigned exists
-		return function(domainId) {
-			for (var i = domains.length - 1; i >= 0; i--) {
-				if (domainId === domains[i].id) {
-					return true;
-				}
-			}
-			return false;
-		};
-	}
-
-	function stepExists(steps) {
-		return function(stepId) {
-			// Verify the step exists
-			for (var i = steps.length - 1; i >= 0; i--) {
-				if (stepId === steps[i].id) {
-					return true;
-				}
-			}
-			return false;
-		};
-	}
+	var fs			= require('fs'),
+		stdin 		= require('get-stdin'),
+		chai		= require('chai'),
+		expect		= chai.expect,
+		Mocha		= require('mocha');
 
 	function testDocument(docData) {
-		// Document must be valid JSON
-		expect(function() { JSON.parse(docData); }).to.not.throw(Error);
-		var doc = JSON.parse(docData);
+		var mocha = new Mocha();
 
-		// Metadata must be object
-		expect(doc).to.have.property('metadata')
-		.that.is.an('object', 'The metadata property must be an object');
+		// Write data to temporary file
+		fs.writeFileSync('testcase.json', docData);
 
-		// Domains is array
-		expect(doc).to.have.property('domains')
-		.that.is.an('array', 'The domains property must be an array');
+		mocha.addFile('./src/mocha.js');
 
-		// Steps is array of at least 2 objects
-		expect(doc).to.have.property('steps')
-		.that.is.an('array', 'The steps property must be an array');
+		mocha.run(function(failures){
+			process.on('exit', function () {
+				process.exit(failures);
+			});
+		});
+	}
 
-		// Domain Validation
-		for (var d = doc.domains.length - 1; d >= 0; d--) {
-			/**
-			1. id is string
-			2. id is unique
-			3. title is string
-			4. subtitle is string
-			**/
-			expect(doc.domains[d]).to.be.an('object', 'Each domain must be an object; domain ' + d + ' is not an object');
-
-			expect(doc.domains[d]).to.have.property('id')
-			.that.is.a('string', 'Domain ' + d + ' id must be a string');
-
-			expect(doc.domains[d]).to.have.property('title')
-			.that.is.a('string', 'Domain ' + d + ' title must be a string');
-
-			expect(doc.domains[d]).to.have.property('subtitle')
-			.that.is.a('string', 'Domain ' + d + ' title must be a string');
-		}
-
-		// Steps validation
-		for (var s = doc.steps.length -1; s >= 0; s--) {
-			var step = doc.steps[s];
-
-			expect(step).to.be.an('object');
-
-			expect(step).to.have.property('id')
-			.that.is.a('string');
-
-			expect(step).to.have.property('title')
-			.that.is.a('string');
-
-			expect(step).to.have.property('type')
-			.that.is.a('string')
-			.and.to.satisfy(stringMatch(['process', 'decision', 'divergent_process', 'wait']));
-
-			if (step.emphasized) {
-				expect(step).to.have.property('emphasized')
-				.that.is.a('boolean');
-			}
-
-			if (step.value_specific) {
-				expect(step).to.have.property('value_specific')
-				.that.is.a('Number')
-				.and.to.satisfy(noDecimals);
-			}
-
-			if (step.value_generic) {
-				expect(step).to.have.property('value_generic')
-				.that.is.a('Number')
-				.and.to.satisfy(noDecimals);
-			}
-
-			if (step.predecessors) {
-				expect(step).to.have.property('predecessors')
-				.that.is.an('array');
-
-				for (var p = step.predecessors.length - 1; p >= 0; p--) {
-					var pred = step.predecessors[p];
-
-					expect(pred).to.have.property('id')
-					.that.is.a('string')
-					.and.to.satisfy(stepExists(doc.steps));
-
-					expect(pred).to.have.property('type')
-					.that.is.a('string')
-					.and.to.satisfy(stringMatch(['normal_relationship', 'loose_temporal_relationship']));
-
-					if (pred.title) {
-						expect(pred).to.have.property('title')
-						.that.is.a('string');
-					}
-				}
-			}
-
-			expect(step).to.have.property('domain')
-			.that.is.an('object');
-
-			var domain = step.domain;
-
-			expect(domain).to.have.property('id')
-			.that.is.a('string')
-			.and.to.satisfy(domainExists(doc.domains));
-
-			expect(domain).to.have.property('region')
-			.that.is.an('object');
-
-			expect(domain.region).to.have.property('type')
-			.that.is.a('string')
-			.and.to.satisfy(stringMatch(['independent', 'surrogate', 'direct_leading', 'direct_shared']));
-
-			if (domain.region.type !== 'independent') {
-				expect(domain.region).to.have.property('with_domain')
-				.that.is.a('string')
-				.and.to.satisfy(domainExists);
-			}
-
-			if (step.problems) {
-				expect(step).to.have.property('problems')
-				.that.is.an('array');
-
-				for (var pr = step.problems.length - 1; pr >= 0; pr--) {
-					expect(step.problems[pr]).to.be.an('object');
-
-					expect(step.problems[pr]).to.have.property('type')
-					.that.is.a('string')
-					.and.to.satisfy(stringMatch(['inconvenient', 'confusing', 'difficult', 'likely_to_fail']));
-
-					expect(step.problems[pr]).to.have.property('description')
-					.that.is.a('string');
-				}
-			}
-		}
+	function cleanup() {
+		fs.unlinkSync('testcase.json');
 	}
 
 	function interpret(rawArgs) {
@@ -188,9 +37,11 @@
 				var data = fs.readFileSync(argv._[i], 'utf8');
 				testDocument(data);
 			}
+			cleanup();
 		} else { // Attempt to use STDIN
 			stdin(function(data) {
 				testDocument(data);
+				cleanup();
 			});
 		}
 	}
