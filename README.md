152c6ea4716a699eed34cfc4c3eb317277850db0
Brent Anderson <me@brentjanderson.com>
2014-12-02 17:27:15 -0700

    Update to include mocha reporter options on the command line. Supports json, other output reporting.

--- README.md
+++ README.md
@@ -18,12 +18,24 @@ Or...
 
 When using file names as parameters, you can chain together multiple files to lint them all together.
 
+## Options
+
+Currently pcnlint will take a list of files to lint (or it will read from stdin if no files are provided).
+
+pcnlint also accepts mocha's test reporter parameters, `--reporter` and `-R` to customize test result output.
+This can be very useful when using pcnlint inside an application, as output can be direct JSON:
+
+    cat pizza-parlor.json | pcnlint --reporter json
+
+All other mocha reporters are available.
+
 ## Todo
 
 - [x] Get the PCN-Spec example to pass
 - [x] Support input from `stdin`
-- [ ] Get other PCN examples to pass
-- [ ] Prettify error output
+- [x] Get other PCN examples to pass
+- [x] Prettify error output
+- [x] Support mocha reporters as an option
 - [ ] Support recursive file input
 
 
--- src/pcnlint.js
+++ src/pcnlint.js
@@ -8,8 +8,8 @@
 		expect		= chai.expect,
 		Mocha		= require('mocha');
 
-	function testDocument(docData) {
-		var mocha = new Mocha();
+	function testDocument(docData, mochaOpts) {
+		var mocha = new Mocha(mochaOpts);
 
 		// Write data to temporary file
 		fs.writeFileSync('testcase.json', docData);
@@ -31,16 +31,26 @@
 		// Determine if we're using stdin or files
 		var argv = require('minimist')(rawArgs.slice(2));
 
+		// Construct mocha arguments
+		// XXXX accept other mocha opts
+		var mochaOpts = {
+			// grep: '',
+			// ui: '',
+			// timeout: '',
+			// bail: '',
+			reporter: argv.R || argv.reporter
+		};
+
 		// File-based input
 		if (argv._.length > 0) {
 			for (var i = argv._.length - 1; i >= 0; i--) {
 				var data = fs.readFileSync(argv._[i], 'utf8');
-				testDocument(data);
+				testDocument(data, mochaOpts);
 			}
 			cleanup();
 		} else { // Attempt to use STDIN
 			stdin(function(data) {
-				testDocument(data);
+				testDocument(data, mochaOpts);
 				cleanup();
 			});
 		}
